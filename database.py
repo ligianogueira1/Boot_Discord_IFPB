6384f792311f7a6003b813075f2175b91a3ec349
Lígia Nogueira <109001008+ligianogueira1@users.noreply.github.com>
2023-06-24 15:03:58 -0300

    update database

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import pandas as pd
 dicas = [
     "Para acessar a rede Wi-Fi do campus Campina Grande, acesse o link e siga os procedimentos: wifi.cge.ifpb.edu.br/",
     "Para ter acesso às últimas notícias do campus Campina Grande, acesse: www.ifpb.edu.br/campinagrande",
-    "Precisa de transporte público para ir ao campus ou voltar para casa? Fique ligado: os ônibus da rota IFPB são: 022, 660 e 220.",
-    "Precisa de transporte público para ir ao campus ou voltar para casa? Confira os horários da rota IFPB:",
+    "Precisa de transporte público para ir ao Campus ou voltar para casa? Fique ligado: os ônibus da rota IFPB são: 022, 660 e 220.",
+    "Precisa de transporte público para ir ao Campus ou voltar para casa? Confira os horários da rota IFPB: https://uploaddeimagens.com.br/images/004/519/078/full/20230517_124730.jpg?1687629742",
     "Para saber mais sobre Assistência Estudantil do Campus, acesse: www.ifpb.edu.br/campinagrande/assistencia-estudantil"
     "Revise regularmente o conteúdo já estudado para fortalecer a memória.",
     "Mantenha uma atitude positiva e persistente em relação aos estudos.",
@@ -34,17 +34,17 @@ dicas = [
     "Não tenha medo de pedir ajuda. Um programador sempre pode contar com seus 'byte-amigos'!",
     "Programar é como construir um castelo de cartas. Certifique-se de ter uma base sólida antes de adicionar mais funcionalidades!",
     "Um programador bem-sucedido sabe que erros são apenas 'features' inesperadas!",
-    "qual é o animal favorito dos programadores? O bit-coin!",
-    "lembre-se que é fundamental contar com uma estrutura sólida de lógica de programação, já que ela é quem te ajuda a ter uma compreensão mais eficiente da formação do código e desenvolver soluções funcionais.",
-    "não decore a sintaxe de uma linguagem de programação, pois ela pode ser consultada a qualquer momento na documentação da linguagem. Invés disso, foque em aprender sobre os fundamentos, isso dará liberdade para implementar soluções em qualquer linguagem.",
-    "utilize papel e caneta; antes de codificar, foque nas ideias e conceitos que cercam o processo. Treine sua mente para resolver esses problemas, isso vai ajudar a traçar o melhor caminho e evitar frustrações com soluções falhas.",
-    "não copie e cole códigos prontos. Isto impede que você analise os fundamentos por trás da lógica desenvolvida.",
-    "antes de codificar, é importante ter diferentes perspectivas sobre o problema. Use ferramentas e técnicas, como fluxogramas, para planejar a estrutura do código antes de escrever e encontrar a solução mais adequada à situação.",
-    "teste sempre o seu código! Ao finalizar uma funcionalidade, tire alguns minutos para clarear a mente e retorne à aplicação. Faça testes buscando refletir sobre os diferentes cenários de uso daquele item e verifique a presença de falhas.",
-    "revisar o código constantemente. De tempos em tempos, volte ao código já implementado e revise-o. Analise se o trecho ainda é necessário, se está funcionando como esperado e se algo pode ser melhorado.",
-    "lembre-se de comentar seu código: reserve um espaço para explicar de forma objetiva a sua linha de raciocínio sobre o que foi construído.",
-    "não repita o código muitas vezes; você pode transformar o trecho que se repete em um componente ou uma função global. Desse modo, esses elementos poderão ser utilizados pela aplicação inteira, mas serão escritos apenas uma vez em um único lugar.",
-    "pular o básico e já partir para o desenvolvimento de sistemas complexos resultará em frustração. Lembre-se: a pressa é inimiga da perfeição. Avance gradualmente com a complexidade dos projetos criados."
+    "Qual é o animal favorito dos programadores? O bit-coin!",
+    "Lembre-se que é fundamental contar com uma estrutura sólida de lógica de programação, já que ela é quem te ajuda a ter uma compreensão mais eficiente da formação do código e desenvolver soluções funcionais.",
+    "Não decore a sintaxe de uma linguagem de programação, pois ela pode ser consultada a qualquer momento na documentação da linguagem. Invés disso, foque em aprender sobre os fundamentos, isso dará liberdade para implementar soluções em qualquer linguagem.",
+    "Utilize papel e caneta; antes de codificar, foque nas ideias e conceitos que cercam o processo. Treine sua mente para resolver esses problemas, isso vai ajudar a traçar o melhor caminho e evitar frustrações com soluções falhas.",
+    "Não copie e cole códigos prontos. Isto impede que você analise os fundamentos por trás da lógica desenvolvida.",
+    "Antes de codificar, é importante ter diferentes perspectivas sobre o problema. Use ferramentas e técnicas, como fluxogramas, para planejar a estrutura do código antes de escrever e encontrar a solução mais adequada à situação.",
+    "Teste sempre o seu código! Ao finalizar uma funcionalidade, tire alguns minutos para clarear a mente e retorne à aplicação. Faça testes buscando refletir sobre os diferentes cenários de uso daquele item e verifique a presença de falhas.",
+    "Revisar o código constantemente. De tempos em tempos, volte ao código já implementado e revise-o. Analise se o trecho ainda é necessário, se está funcionando como esperado e se algo pode ser melhorado.",
+    "Lembre-se de comentar seu código: reserve um espaço para explicar de forma objetiva a sua linha de raciocínio sobre o que foi construído.",
+    "Não repita o código muitas vezes; você pode transformar o trecho que se repete em um componente ou uma função global. Desse modo, esses elementos poderão ser utilizados pela aplicação inteira, mas serão escritos apenas uma vez em um único lugar.",
+    "Pular o básico e já partir para o desenvolvimento de sistemas complexos resultará em frustração. Lembre-se: a pressa é inimiga da perfeição. Avance gradualmente com a complexidade dos projetos criados."
 ]
 
 students = pd.read_csv('data/alunos.csv')
